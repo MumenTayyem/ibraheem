09beef64c45b820123738794c9e2f0eaa4f8c6cf
Mumen Tayyem <mumen_tayyem@outlook.com>
2024-12-04 06:40:40 +0400

    Syntax 1

---
+++ public/js/authorization-validator.js
@@ -0,0 +1,15 @@
+function authorizationValidator(config){
+    let token = localStorage.getItem("token");
+
+    let decoded = jwt_decode(token);
+
+    for (let i = 0; i< config.allowed_roles.length; i++){
+        let currentRole = config.allowed_roles[i];
+
+        if (decoded.Role.indexOf(currentRole) > -1){
+            return true;
+        }
+    }
+
+    return false;
+}
